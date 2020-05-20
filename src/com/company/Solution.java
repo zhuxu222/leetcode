0d3dcc6db0959f065a8d18abb9ab847bf5b06ce4
zhuxu <zhuxu@douyu.tv>
2020-05-20 13:33:49 +0800

    findTheLongestSubstring

--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -244,8 +244,34 @@ class Solution {
         return s.substring((maxCenter-maxRediu+1)/2,(maxCenter+maxRediu-1)/2);
     }
     public static int findTheLongestSubstring(String s) {
+        int[] pos=new int[1<<5];
+        Arrays.fill(pos,-1);
+        int status=0;
+        int ans=0;
+        pos[0]=0;
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            if(c=='a'){
+                status^=(1<<0);
+            }else if(c=='e'){
+                status^=(1<<1);
+            }else if(c=='i'){
+                status^=(1<<2);
+            }else if(c=='o'){
+                status^=(1<<3);
+            }else if(c=='u'){
+                status^=(1<<4);
+            }
+            if(pos[status]>=0){
+                ans=Math.max(ans,i+1-pos[status]);
+            }else{
+                pos[status]=i+1;
+            }
+        }
+        return ans;
+    }
+    public static int findKthNumber(int m, int n, int k) {
 
     }
-
 
 }
